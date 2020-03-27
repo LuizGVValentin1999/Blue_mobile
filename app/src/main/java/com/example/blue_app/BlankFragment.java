f3d3c00ae1809f16df40e69da64cb2dc2223c6f7
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2020-03-26 22:46:28 -0300

    Mobile atualizado

---
+++ app/src/main/java/com/example/blue_app/BlankFragment.java
@@ -0,0 +1,33 @@
+package com.example.blue_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends Fragment {
+
+    Button button_cadastro;
+    TextView textView_cadastro;
+
+
+    private Button btnTeste;
+    public void onAttach(BlankFragment activity) {
+
+
+
+        }
+    }
+
+}
